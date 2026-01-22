6b379bc9be130f1c87c9525f8ff570214e1a950c
Pedro Lucas <pedrolucasalmeidanogueira@gmail.com>
2026-01-22 14:07:06 -0300

    criação do model Base

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    criacao_em = models.DateTimeField(auto_now_add=True)
+    atualizacao_em = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
