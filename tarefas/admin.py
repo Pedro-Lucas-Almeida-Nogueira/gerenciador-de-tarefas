372ff4d5b83cfff43d26d1c8671f2b44096ab80c
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-01-22 15:36:54 -0300

    remoção dos acentos no list_display em admin.py

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -3,8 +3,8 @@ from .models import Usuario, Tarefa
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ("nome", "email", "criação", "atualização")
+    list_display = ("nome", "email", "criacao", "atualizacao")
 
 @admin.register(Tarefa)
 class TarefaAdmin(admin.ModelAdmin):
-    list_display = ("título", "descrição", "criação", "atualização")
+    list_display = ("titulo", "descricao", "criacao", "atualizacao")
