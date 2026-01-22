d69c25bf18978b371bf50ac4aadde606d7ac589f
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-01-22 15:31:49 -0300

    registro dos models Usuario e Tarefa no admin.py

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Usuario, Tarefa
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("nome", "email", "criação", "atualização")
+
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ("título", "descrição", "criação", "atualização")
