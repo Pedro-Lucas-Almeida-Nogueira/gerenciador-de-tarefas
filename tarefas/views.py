9eb4e38263a4d31b59c1f291c162bd4084ac5960
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-01-23 11:09:21 -0300

    criação do método get da UsuarioApiView

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Usuario, Tarefa
+from .serializers import UsuarioSerializer, TarefaSerializer
 
-# Create your views here.
+class UsuarioApiView(APIView):
+    
+    def get(self, request):
+        usuarios = Usuario.objects.all()
+        serializer = UsuarioSerializer(usuarios, many=True)
+        return Response(serializer.data)
