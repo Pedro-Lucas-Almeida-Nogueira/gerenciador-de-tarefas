380ac14bf1285991311f820e990e31cd8306b527
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-01-23 09:14:53 -0300

    criação do TarefaSerializer

--- tarefas/serializers.py
+++ tarefas/serializers.py
@@ -6,3 +6,9 @@ class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         fields = ("nome", "email", "criacao_em", "atualizacao_em")
+
+class TarefaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tarefa
+        fields = ("titulo", "descricao", "criacao_em", "atualizacao_em")
